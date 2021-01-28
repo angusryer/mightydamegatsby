800c40a9e4b5ec9dfb0558bc91bc7e722273f9ad
Angus Ryer <angus@enabled.company>
2021-01-28 00:00:39 -0500

    mostly backend stripe integration work

--- amplify/backend/function/mightydamegatsbyStripeFunction/src/app.js
+++ amplify/backend/function/mightydamegatsbyStripeFunction/src/app.js
@@ -1,34 +1,34 @@
 // https://stripe.com/docs/payments/without-card-authentication
-const stripe = require("stripe")(process.env.SK_TEST);
+const stripe = require("stripe")(process.env.SK_TEST)
 
 exports.handler = async (event, context, callback) => {
-  console.log("THE EVENT ===> ", event);
+  console.log("THE EVENT ===> ", event)
   if (!event.body || event.httpMethod !== "POST") {
     return {
       statusCode: 400,
       body: JSON.stringify({
         status: "invalid http method",
       }),
-    };
+    }
   }
 
-  const order = JSON.parse(event.body);
+  const order = JSON.parse(event.body)
 
   const getTaxRate = (provinceCode) => {
-      switch (provinceCode) {
-        case "ON":
-          return 0.13;
-        default:
-          return 0;
-      }
-    };
-  
+    switch (provinceCode) {
+      case "ON":
+        return 0.13
+      default:
+        return 0
+    }
+  }
+
   const calculateOrderAmount = (items, provinceCode) => {
     const itemsTotal = items.reduce((accumulatedValue, nextItem) => {
-      return accumulatedValue + (nextItem.price * nextItem.quantity);
-    }, 0);
-    return Math.round(itemsTotal * (1 + getTaxRate(provinceCode)) * 100);
-  };
+      return accumulatedValue + nextItem.price * nextItem.quantity
+    }, 0)
+    return Math.round(itemsTotal * (1 + getTaxRate(provinceCode)) * 100)
+  }
 
   try {
     const intent = await stripe.paymentIntents.create({
@@ -44,78 +44,80 @@ exports.handler = async (event, context, callback) => {
       // customer, like two-factor authentication, Stripe will error
       // and you will need to prompt them for a new payment method.
       error_on_requires_action: true,
-    });
+    })
 
     if (intent.status === "succeeded") {
       // This creates a new Customer and attaches the PaymentMethod in one API call.
-      const customers = await stripe.customers.list({ email: order.email });
-      let existingCustomer;
-      let customer;
+      const customers = await stripe.customers.list({ email: order.email })
+      let existingCustomer
+      let customer
       if (customers.length > 0) {
-        existingCustomer = customers.find(cust => cust.email === order.email);
+        existingCustomer = customers.find((cust) => cust.email === order.email)
       }
       if (customers.length == 0) {
         customer = await stripe.customers.create({
           payment_method: intent.payment_method,
           email: order.email,
           address: order.address,
-        });
+        })
       }
-      await stripe.paymentMethods.attach(
-        intent.payment_method,
-        {customer: customer.id || existingCustomer.id}
-      );
+      await stripe.paymentMethods.attach(intent.payment_method, {
+        customer: customer.id || existingCustomer.id,
+      })
       // Handle post-payment fulfillment
-      console.log(`Created Payment: ${intent.id} for Customer: ${customer.id || existingCustomer.id}`);
+      console.log(
+        `Created Payment: ${intent.id} for Customer: ${
+          customer.id || existingCustomer.id
+        }`
+      )
       // Now ship those goodies
       // await inventoryAPI.ship(order)
-      
+
       const body = {
         client_secret: intent.client_secret,
-        receipt_url: intent.charges.data[0].receipt_url
-      };
-      
+        receipt_url: intent.charges.data[0].receipt_url,
+      }
+
       const response = {
         statusCode: 200,
         body: JSON.stringify(body),
-        isBase64Encoded: false
-      };
+        isBase64Encoded: false,
+      }
 
       // return response;
-      callback(null, response);
-
+      callback(null, response)
     } else {
       // Any other status would be unexpected, so error
-      console.log({ error: "Unexpected status " + intent.status });
+      console.log({ error: "Unexpected status " + intent.status })
       const response = {
         statusCode: 400,
-        body: JSON.stringify({error: intent.status}),
-        isBase64Encoded: false
-      };
+        body: JSON.stringify({ error: intent.status }),
+        isBase64Encoded: false,
+      }
       // return response;
-      callback(response, null);
+      callback(response, null)
     }
   } catch (e) {
     if (e.type === "StripeCardError") {
       const response = {
         statusCode: 400,
         body: JSON.stringify(e),
-        isBase64Encoded: false
-      };
+        isBase64Encoded: false,
+      }
       // Display error to customer
-      console.log(JSON.stringify(e));
+      console.log(JSON.stringify(e))
       // return response;
-      callback(response, null);
+      callback(response, null)
     } else {
       // Something else happened
-      console.log(JSON.stringify(e));
+      console.log(JSON.stringify(e.type))
       const response = {
         statusCode: 400,
-        body: JSON.stringify(e),
-        isBase64Encoded: false
-      };
+        body: JSON.stringify(e.type),
+        isBase64Encoded: false,
+      }
       // return response;
-      callback(response, null);
+      callback(response, null)
     }
   }
-};
+}
